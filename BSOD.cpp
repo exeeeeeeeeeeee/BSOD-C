7728170e5ada1f316d54eb9e8bdc31af7b280883
haruk <104354987+exeeeeeeeeeeee@users.noreply.github.com>
2024-12-17 00:04:56 +0900

    Add source

---
+++ BSOD.cpp
@@ -0,0 +1,26 @@
+﻿#include <cstdio>
+#include <Windows.h>
+
+typedef NTSTATUS(NTAPI* pdef_NtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask OPTIONAL, PULONG_PTR Parameters, ULONG ResponseOption, PULONG Response);
+typedef NTSTATUS(NTAPI* pdef_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
+
+int GenerateBSOD(int errorCode)
+{
+    printf("Generating BSOD...");
+
+    BOOLEAN bEnabled;
+    ULONG uResp;
+
+    LPVOID lpFuncAddress = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
+    LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtRaiseHardError");
+
+    pdef_RtlAdjustPrivilege NtCall = (pdef_RtlAdjustPrivilege)lpFuncAddress;
+    pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
+
+    //Enable shutdown privilege
+    NtCall(19, TRUE, FALSE, &bEnabled);
+    //Generate BSOD
+    NtCall2(errorCode, 0, 0, 0, 6, &uResp);
+    
+    return 0;
+}
